f633de7771b8976e671b3059820a3623059297e5
dariatymoshenko <70763801+dariatymoshenko@users.noreply.github.com>
2020-12-14 19:35:02 +0200

    Add files via upload

--- dasha.php
+++ dasha.php
@@ -1,50 +1,40 @@
 <!DOCTYPE html>
 <html>
- <head>
-  <meta charset="utf-8" />
-  <title>Catalog</title>
-  </head>
- <body>
+<head>
+    <meta charset="utf-8" />
+    <title>Catalog</title>
+</head>
+<body>
+<h1>Мой каталог</h1>
 <form action="" method="post" enctype="multipart/form-data" >
-    <input name="add" type="file" />
-    <input name="save" type="submit" value="Сохранить" />
+    <p><input name="add" type="file" /></p>
+    </p><input name="save" type="submit" value="Сохранить" /></p>
 </form>
- </body>
-</html>
-<?
-     if(isset($_FILES["add"])) 
-{ 
-$myfile = $_FILES["add"]["tmp_name"]; 
-$myfile_name = $_FILES["add"]["name"]; 
-$myfile_size = $_FILES["add"]["size"]; 
-$myfile_type = $_FILES["add"]["type"]; 
-$error_flag = $_FILES["add"]["error"];
-
-// Если ошибок не было 
-if($error_flag == 0) 
+<?php
+if(isset($_FILES["add"]))
 {
+    $myfile = $_FILES["add"]["tmp_name"];
+    $myfile_name = $_FILES["add"]["name"];
+    $error_flag = $_FILES["add"]["error"];
 
-$upload='./my_files/' . $myfile_name;
+// Если ошибок не было
+    if($error_flag == 0)
+    {
 
- copy($_FILES['add']['tmp_name'], $upload);
+        $upload='./files/' . $myfile_name;
 
+        copy($_FILES['add']['tmp_name'], $upload);
+
+    }
 }
-}
-$dir = "./my_files/";
+$dir = __DIR__ . "/files/";
 $files = scandir($dir);
-?>
-<form action="download.php" method="POST"  >
-<?
-foreach ($files as $n_files)
-{
-    ?>
-    <input name="file" type="radio" value="<?php echo $n_files; ?>">
-    <?
-    echo $n_files . '<br>';
+foreach (array_diff($files, ['..', '.']) as $file)
+    {
+        echo <<< OUT
+<p><b>$file</b> <a href="/dashadasha/files/$file">Скачать</a></p>
+OUT;
 
-}
-    ?>
-    <input name="download" type="submit" value="Сохранить" />
-    </form>
-    <?
+    }
 ?>
+</html>
